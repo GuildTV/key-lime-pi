ef76f6b2c70f5b10367fc120b9b62c701b94d489
Julian Waller <julus@julus-TheA.(none)>
2012-07-09 18:50:25 +0100

    re-enabled enforce command commented SubtitleRenderer code

--- SubtitleRenderer.cpp
+++ SubtitleRenderer.cpp
@@ -23,24 +23,30 @@
 #include <cassert>
 
 class BoxRenderer {
+  //openVG path and paint? TODO
   VGPath path_;
   VGPaint paint_;
 
 public:
+  //create box render and assign opacity
   BoxRenderer(unsigned int opacity) {
+	//create path for renderer
     path_ = vgCreatePath(VG_PATH_FORMAT_STANDARD, VG_PATH_DATATYPE_F,
                         1.0, 0.0,
                         0, 0,
                         VG_PATH_CAPABILITY_ALL);
     assert(path_);
 
+	//create painter
     paint_ = vgCreatePaint();
     assert(paint_);
 
+	//set color of the painter
     vgSetColor(paint_, opacity);
     assert(!vgGetError());
   }
 
+  //destroy renderer
   ~BoxRenderer() {
     vgDestroyPath(path_);
     assert(!vgGetError());
@@ -48,28 +54,34 @@ public:
     assert(!vgGetError());
   }
 
+  //clear the path
   void clear() {
     vgClearPath(path_, VG_PATH_CAPABILITY_ALL);
     assert(!vgGetError());
   }
 
+  //add a rectangle onto the draw path
   void push(int x, int y, int width, int height) {
     assert(width >= 0);
     assert(height >= 0);
 
+	//add rectangle to path
     vguRect(path_, x, y, width, height);
     assert(!vgGetError());
   };
 
   void render() {
+	//set the paint beign used
     vgSetPaint(paint_, VG_FILL_PATH);
     assert(!vgGetError());
 
+	//draw the path specified
     vgDrawPath(path_, VG_FILL_PATH);
     assert(!vgGetError());
   }
 };
 
+//load a glyph?? TODO
 void SubtitleRenderer::load_glyph(char32_t codepoint) {
   VGfloat escapement[2]{};
 
@@ -170,28 +182,38 @@ void SubtitleRenderer::load_glyph(char32_t codepoint) {
   load_glyph_internal(ft_face_, vg_font_border_, true);
 }
 
+//calculate width of the specified text
 int SubtitleRenderer::get_text_width(const std::vector<InternalChar>& text) {
   int width = 0;
+  //foreach letter
   for (auto c = text.begin(); c != text.end(); ++c) {
+	//increment width
     width += c->italic ? (assert(0), 0)
                        : glyphs_.at(c->codepoint).advance;
   }
   return width;
 }
 
+//get the 'InternalChars' for a string
 std::vector<SubtitleRenderer::InternalChar> SubtitleRenderer::
 get_internal_chars(const std::string& str) {
+	//create array
   std::vector<InternalChar> internal_chars;
   try {
+	  
     bool italic{};
+	//loop through string
     for (auto p = &str[0], e = p + str.size(); p != e;) {
+		//get char from string
       auto const cp = folly::decodeUtf8(p, e);
+	  //push char to array
       internal_chars.push_back(InternalChar({cp, italic}));
     }
   } catch(...) {}
   return internal_chars;
 }
 
+//prepare the glyphs to be drawn
 void SubtitleRenderer::
 prepare_glyphs(const std::vector<InternalChar>& text) {
   for (auto c = text.begin(); c != text.end(); ++c) {
@@ -201,31 +223,40 @@ prepare_glyphs(const std::vector<InternalChar>& text) {
   }
 }
 
+//draw text to the screen
 void SubtitleRenderer::
 draw_text(VGFont font, VGFont italic_font,
           const std::vector<SubtitleRenderer::InternalChar>& text,
           int x, int y,
           unsigned int lightness) {
+  //create paint to use
   VGPaint paint = vgCreatePaint();
   assert(paint);
 
+  //set color
   vgSetColor(paint, (lightness<<8) | (lightness<<16) | (lightness<<24) | 0xFF);
   assert(!vgGetError());
 
+  //set paint 
   vgSetPaint(paint, VG_FILL_PATH);
   assert(!vgGetError());
-
+  
+  //destroy paint
   vgDestroyPaint(paint);
   assert(!vgGetError());
 
+  //set drawing mode
   vgSeti(VG_IMAGE_MODE, VG_DRAW_IMAGE_MULTIPLY);
   assert(!vgGetError());
 
+  //??
   VGfloat pos[] = {static_cast<VGfloat>(x), static_cast<VGfloat>(y)};
 
+  //set glyph origin??
   vgSetfv(VG_GLYPH_ORIGIN, 2, pos);
   assert(!vgGetError());
 
+  //for each character, draw the glyph
   for (auto c = text.begin(); c != text.end(); ++c) {
     vgDrawGlyph(c->italic ? italic_font : font,
                 c->codepoint,
@@ -235,11 +266,12 @@ draw_text(VGFont font, VGFont italic_font,
   }
 }
 
-
+//destroy the renderer
 SubtitleRenderer::~SubtitleRenderer() {
   destroy();
 }
 
+//create the renderer
 SubtitleRenderer::
 SubtitleRenderer(int layer,
                  const std::string& font_path,
@@ -248,6 +280,7 @@ SubtitleRenderer(int layer,
                  float margin_bottom,
                  unsigned int white_level,
                  unsigned int box_opacity):
+//set no value
 prepared_(),
 dispman_element_(),
 dispman_display_(),
@@ -266,13 +299,17 @@ ft_stroker_(),
 line_height_(),
 box_offset_(),
 box_h_padding_(),
+//assign values
 margin_left_(margin_left),
 margin_bottom_(margin_bottom),
 white_level_(white_level),
 box_opacity_(box_opacity) {
   try {
+	  //initialise the window
     initialize_window(layer);
+	//initialise egl
     initialize_egl();
+	//intialise the fonts
     initialize_fonts(font_path, font_size);
   } catch (...) {
     destroy();
@@ -280,12 +317,14 @@ box_opacity_(box_opacity) {
   }
 }
 
+//destroy
 void SubtitleRenderer::destroy() {
   destroy_fonts();
   destroy_egl();
   destroy_window();
 }
 
+//initialise fonts...
 void SubtitleRenderer::
 initialize_fonts(const std::string& font_path, float font_size) {
   ENFORCE(!FT_Init_FreeType(&ft_library_));
@@ -337,6 +376,7 @@ initialize_fonts(const std::string& font_path, float font_size) {
   create_vg_font(vg_font_border_);
 }
 
+//destroy fonts
 void SubtitleRenderer::destroy_fonts() {
   if (ft_library_) {
     auto error = FT_Done_FreeType(ft_library_);
@@ -347,32 +387,39 @@ void SubtitleRenderer::destroy_fonts() {
   }
 } 
 
+//setup window
 void SubtitleRenderer::initialize_window(int layer) {
+	//check display has size
   ENFORCE(graphics_get_display_size(0 /* LCD */, &screen_width_, &screen_height_)
           >= 0);
 
+//define dest size as a rectangle
   VC_RECT_T dst_rect;
   dst_rect.x = 0;
   dst_rect.y = 0;
   dst_rect.width = screen_width_;
   dst_rect.height = screen_height_;
      
+//define src size as a rectangle
   VC_RECT_T src_rect;
   src_rect.x = 0;
   src_rect.y = 0;
   src_rect.width = screen_width_ << 16;
   src_rect.height = screen_height_ << 16;        
 
+//open display 0
   dispman_display_ = vc_dispmanx_display_open(0 /* LCD */);
   ENFORCE(dispman_display_);
 
   {
+	  //start update display loop??
     auto dispman_update = vc_dispmanx_update_start(0);
     ENFORCE(dispman_update);
     SCOPE_EXIT {
       ENFORCE(!vc_dispmanx_update_submit_sync(dispman_update));
     };
 
+	//create display element
     dispman_element_ =
         vc_dispmanx_element_add(dispman_update,
                                 dispman_display_,
@@ -388,6 +435,7 @@ void SubtitleRenderer::initialize_window(int layer) {
   }
 }
 
+//destroy the window TODO - no clue how
 void SubtitleRenderer::destroy_window() {  
   if (dispman_element_) {
     auto dispman_update = vc_dispmanx_update_start(0);
@@ -412,6 +460,7 @@ void SubtitleRenderer::destroy_window() {
   }
 }
 
+//initialise egl
 void SubtitleRenderer::initialize_egl() {
   // get an EGL display connection
   display_ = eglGetDisplay(EGL_DEFAULT_DISPLAY);
@@ -432,16 +481,20 @@ void SubtitleRenderer::initialize_egl() {
   EGLConfig config{};
   EGLint num_config{};
 
+	//load config
   ENFORCE(eglChooseConfig(display_, attribute_list, &config, 1, &num_config));
   ENFORCE(num_config);
 
+	//bing openvg api
   ENFORCE(eglBindAPI(EGL_OPENVG_API));
 
+//egl window
   static EGL_DISPMANX_WINDOW_T nativewindow;
   nativewindow.element = dispman_element_;
   nativewindow.width = screen_width_;
   nativewindow.height = screen_height_;
      
+	 //create window surface
   surface_ = eglCreateWindowSurface(display_, config, &nativewindow, NULL);
   ENFORCE(surface_);
 
@@ -449,25 +502,31 @@ void SubtitleRenderer::initialize_egl() {
   context_ = eglCreateContext(display_, config, EGL_NO_CONTEXT, NULL);
   ENFORCE(context_);
 
+//make it current
   auto result = eglMakeCurrent(display_, surface_, surface_, context_);
   assert(result);
 }
 
+//destroy egl
 void SubtitleRenderer::destroy_egl() {
   if (display_) {
+	  //set null as current display
     auto result =
       eglMakeCurrent(display_, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
     assert(result);
 
+	//terminate display 
     result = eglTerminate(display_);
     assert(result);
 
+	//clear display stuff
     context_ = {};
     surface_ = {};
     display_ = {};
   }
 }
 
+//prepare renderer with text TODO??
 void SubtitleRenderer::prepare(const std::vector<std::string>& text_lines) {
   const int n_lines = text_lines.size();
 
@@ -491,27 +550,35 @@ void SubtitleRenderer::prepare(const std::vector<std::string>& text_lines) {
   prepared_ = true;
 }
 
+//clear screen
 void SubtitleRenderer::clear() {
+  //clear screen
   vgClear(0, 0, screen_width_, screen_height_);
   assert(!vgGetError());
 }
 
+//draw
 void SubtitleRenderer::draw() {
   clear();
 
+  //count number of lines to draw
   const auto n_lines = internal_lines_.size();
 
   {
+	//create a boxrenderer
     BoxRenderer box_renderer(box_opacity_);
+	//for each line, make box renderer draw a lil more
     for (size_t i = 0; i < n_lines; ++i) {
       box_renderer.push(line_positions_[i].first - box_h_padding_,
                         line_positions_[i].second + box_offset_,
                         line_widths_[i] + box_h_padding_*2,
                         line_height_);
     }
+	//render
     box_renderer.render();
   }
 
+  //draw text borders
   for (size_t i = 0; i < n_lines; ++i) {
     draw_text(vg_font_border_, vg_font_italic_border_,
               internal_lines_[i],
@@ -519,6 +586,7 @@ void SubtitleRenderer::draw() {
               0);
   }
 
+  //draw text
   for (size_t i = 0; i < n_lines; ++i) {
     draw_text(vg_font_, vg_font_italic_,
               internal_lines_[i],
@@ -526,9 +594,11 @@ void SubtitleRenderer::draw() {
               white_level_);
   }
 
+  //not prepared for next render
   prepared_ = false;
 }
 
+//show on screen
 void SubtitleRenderer::swap_buffers() {
   EGLBoolean result = eglSwapBuffers(display_, surface_);
   assert(result);
